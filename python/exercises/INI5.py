d8e10a2fa56a82cc0c2e80f07f7d0240f02730d9
ZonesTissyc <ZonesTissyc@github.com>
2024-10-29 13:09:00 +0800

    new file:   dh.py

---
+++ python/exercises/INI5.py
@@ -0,0 +1,9 @@
+## Given: A file containing at most 1000 lines.
+## Return: A file containing all the even-numbered lines from the original file. Assume 1-based numbering of lines.
+## Simple is INI5-.txt
+
+# open and read the simple text.
+
+f = open('INI5-test.txt','r')
+
+f.readline()[]
