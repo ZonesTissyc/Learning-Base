eb08d987ccd2aceef16b823b55c1746039c771b5
ZonesTissyc <102893132+ZonesTissyc@users.noreply.github.com>
2025-03-15 13:30:13 +0800

    更新 gcd.py

--- python/exercises/NCRE-最大公约数.py
+++ python/exercises/NCRE-最大公约数.py
@@ -1,4 +1,18 @@
 # 编写一个函数，计算并返回两个数的最大公约数。
 
-def find(a,b):
+"""
+采用辗转相除法： 设a>b，则a/b的余数为c，若c=0，则b为最大公约数，否则，a=b，b=c，再求a/b的余数，直到余数为0，此时的b即为最大公约数。
+
+"""
+
+def gcd(a,b):
+    while b != 0:
+        temp = a % b
+        a = b
+        b = temp
+    return a
+
+# 测试
+
+print(gcd(10,20))
     
